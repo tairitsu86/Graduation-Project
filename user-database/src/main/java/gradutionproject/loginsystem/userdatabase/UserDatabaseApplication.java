894ca2641ecc2c5c684173a123d2450df59423e7
tairitsu86 <113744248+tairitsu86@users.noreply.github.com>
2023-08-05 11:30:28 +0800

    Add files via upload

---
+++ user-database/src/main/java/gradutionproject/loginsystem/userdatabase/UserDatabaseApplication.java
@@ -0,0 +1,13 @@
+package gradutionproject.loginsystem.userdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDatabaseApplication.class, args);
+	}
+
+}
---
+++ user-database/src/test/java/gradutionproject/loginsystem/userdatabase/UserDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package gradutionproject.loginsystem.userdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
