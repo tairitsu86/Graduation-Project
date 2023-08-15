01f813f92f007e2916fd39a7e97ec958db0a645f
tairitsu86 <113744248+tairitsu86@users.noreply.github.com>
2023-08-15 13:29:18 +0800

    Add files via upload

---
+++ webhook-handler/src/test/java/gradutionProject/IMUISystem/webhookhandler/WebhookHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package gradutionProject.IMUISystem.webhookhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
