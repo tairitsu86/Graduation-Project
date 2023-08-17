475c6d35353d5e14d7bdfcc01c332509a63255c3
tairitsu86 <benimiku9@gmail.com>
2023-08-17 15:40:55 +0800

    delete unused import

--- login-tracker/src/main/java/gradutionProject/IMUISystem/loginTracker/repository/LoginUserRepositoryServiceImpl.java
+++ login-tracker/src/main/java/gradutionProject/IMUISystem/loginTracker/repository/LoginUserRepositoryServiceImpl.java
@@ -4,8 +4,6 @@ import gradutionProject.IMUISystem.loginTracker.api.exception.LoginUserNotExistE
 import gradutionProject.IMUISystem.loginTracker.entity.IMUserData;
 import gradutionProject.IMUISystem.loginTracker.entity.LoginUser;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
