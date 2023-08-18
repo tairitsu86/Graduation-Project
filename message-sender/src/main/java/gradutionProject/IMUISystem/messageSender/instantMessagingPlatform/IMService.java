ad7e1b7e688f8c372f7122880d0832327b0e2173
tairitsu86 <benimiku9@gmail.com>
2023-08-18 14:35:34 +0800

    add and implement IM service

---
+++ message-sender/src/main/java/gradutionProject/IMUISystem/messageSender/instantMessagingPlatform/IMService.java
@@ -0,0 +1,5 @@
+package gradutionProject.IMUISystem.messageSender.instantMessagingPlatform;
+
+public interface IMService {
+    void SendTextMessage(String userId,String message);
+}
---
+++ message-sender/src/main/java/gradutionProject/IMUISystem/messageSender/instantMessagingPlatform/line/LineService.java
@@ -0,0 +1,32 @@
+package gradutionProject.IMUISystem.messageSender.instantMessagingPlatform.line;
+
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.PushMessage;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
+import gradutionProject.IMUISystem.messageSender.instantMessagingPlatform.IMService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.ExecutionException;
+
+public class LineService implements IMService {
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
+    @Override
+    public void SendTextMessage(String userId, String message) {
+        TextMessage textMessage = new TextMessage(message);
+        pushMessage(userId,textMessage);
+    }
+    public void pushMessage(String userId, Message message){
+        PushMessage pushMessage = new PushMessage(userId, message);
+        System.out.println(pushMessage.getTo()+"\n"+pushMessage.getMessages());
+        BotApiResponse response = null;
+        try {
+            response = lineMessagingClient.pushMessage(pushMessage).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
