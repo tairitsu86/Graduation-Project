08f5d74bca1397b2ad0685c69485ce6d423c0f79
tairitsu86 <benimiku9@gmail.com>
2023-08-18 14:10:41 +0800

    add rest template bean

--- message-sender/src/main/java/gradutionProject/IMUISystem/messageSender/MessageSenderApplication.java
+++ message-sender/src/main/java/gradutionProject/IMUISystem/messageSender/MessageSenderApplication.java
@@ -2,6 +2,8 @@ package gradutionProject.IMUISystem.messageSender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MessageSenderApplication {
@@ -9,5 +11,8 @@ public class MessageSenderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MessageSenderApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate createRestTemplateBean(){
+		return new RestTemplate();
+	}
 }
