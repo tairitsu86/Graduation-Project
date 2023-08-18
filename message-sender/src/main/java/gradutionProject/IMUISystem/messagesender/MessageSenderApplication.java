f8f4c3471806a3e2ce595344c2d9e692815cc726
tairitsu86 <113744248+tairitsu86@users.noreply.github.com>
2023-08-18 13:11:05 +0800

    Add files via upload

---
+++ message-sender/src/main/java/gradutionProject/IMUISystem/messagesender/MessageSenderApplication.java
@@ -0,0 +1,13 @@
+package gradutionProject.IMUISystem.messagesender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageSenderApplication.class, args);
+	}
+
+}
---
+++ message-sender/src/test/java/gradutionProject/IMUISystem/messagesender/MessageSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package gradutionProject.IMUISystem.messagesender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
