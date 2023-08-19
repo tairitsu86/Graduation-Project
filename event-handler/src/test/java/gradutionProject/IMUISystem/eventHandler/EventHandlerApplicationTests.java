5d3c0a20290f110a66c7f690ddfe8dbcef6b0f49
tairitsu86 <benimiku9@gmail.com>
2023-08-19 18:18:18 +0800

    alter dir name

--- event-handler/src/test/java/gradutionProject/IMUISystem/eventHandler/EventHandlerApplicationTests.java
+++ event-handler/src/test/java/gradutionProject/IMUISystem/eventHandler/EventHandlerApplicationTests.java
@@ -1,4 +1,4 @@
-package gradutionProject.IMUISystem.eventhandler;
+package gradutionProject.IMUISystem.eventHandler;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
