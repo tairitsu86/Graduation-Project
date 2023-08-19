3dd4a97c189d3488d3ee5f8af75b18d76e3abdfb
tairitsu86 <113744248+tairitsu86@users.noreply.github.com>
2023-08-19 16:55:42 +0800

    Add files via upload

---
+++ event-handler/src/main/java/gradutionProject/IMUISystem/eventhandler/EventHandlerApplication.java
@@ -0,0 +1,13 @@
+package gradutionProject.IMUISystem.eventhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventHandlerApplication.class, args);
+	}
+
+}
---
+++ event-handler/src/test/java/gradutionProject/IMUISystem/eventhandler/EventHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package gradutionProject.IMUISystem.eventhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
