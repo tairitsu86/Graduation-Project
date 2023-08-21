aa5014de937c8af389ab4728cba8a13ad891cd26
tairitsu86 <benimiku9@gmail.com>
2023-08-21 13:29:39 +0800

    add mq listener to get the user message

---
+++ event-handler/src/main/java/gradutionProject/IMUISystem/eventHandler/rabbitMQ/LoginEventListener.java
@@ -0,0 +1,35 @@
+package gradutionProject.IMUISystem.eventHandler.rabbitMQ;
+
+import gradutionProject.IMUISystem.eventHandler.dto.MessageEventDto;
+import gradutionProject.IMUISystem.eventHandler.handler.EventHandler;
+import gradutionProject.IMUISystem.eventHandler.handler.MessageHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import static gradutionProject.IMUISystem.eventHandler.rabbitMQ.RabbitmqConfig.queueName;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class LoginEventListener {
+    private final MessageHandler messageHandler;
+    @RabbitListener(queues={queueName})
+    public void receive(MessageEventDto messageEventDto) {
+        log.info("Message event: "+messageEventDto.toString());
+        messageHandler.checkUser(messageEventDto.getImUserData(),messageEventDto.getMessage());
+    }
+
+    /* test data
+    {
+      "imUserData": {
+        "platform": "LINE",
+        "userId": "123"
+      },
+      "username": "OAO",
+      "state": "LOGIN"
+    }
+    
+    */
+}
---
+++ event-handler/src/main/java/gradutionProject/IMUISystem/eventHandler/rabbitMQ/RabbitmqConfig.java
@@ -0,0 +1,23 @@
+package gradutionProject.IMUISystem.eventHandler.rabbitMQ;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class RabbitmqConfig {
+    public static final String queueName = "IM-UI/User-event";
+    @Bean
+    public MessageConverter jsonMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName,false);
+    }
+}
