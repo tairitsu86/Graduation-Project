70901b57e488729d385f90e302ffd2e5b5a9c1d3
tairitsu86 <benimiku9@gmail.com>
2023-08-22 14:10:19 +0800

    alter mq exchange and routing key

--- webhook-handler/src/main/java/gradutionProject/IMUISystem/webhookHandler/rabbitMQ/MQEventPublisherImpl.java
+++ webhook-handler/src/main/java/gradutionProject/IMUISystem/webhookHandler/rabbitMQ/MQEventPublisherImpl.java
@@ -5,12 +5,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
+import static gradutionProject.IMUISystem.webhookHandler.rabbitMQ.RabbitmqConfig.topicExchangeName;
+import static gradutionProject.IMUISystem.webhookHandler.rabbitMQ.RabbitmqConfig.userEventQueue;
+
 @Service
 @RequiredArgsConstructor
 public class MQEventPublisherImpl implements MQEventPublisher{
     private final RabbitTemplate rabbitTemplate;
     @Override
     public void publishEvent(MessageEventDto event) {
-        rabbitTemplate.convertAndSend(RabbitmqConfig.topicExchangeName, "im-ui.webhook-handler", event);
+        rabbitTemplate.convertAndSend(topicExchangeName,userEventQueue , event);
     }
 }
