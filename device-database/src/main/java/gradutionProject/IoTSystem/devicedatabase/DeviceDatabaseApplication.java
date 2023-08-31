48506ea18a2693b16b5a459475473ebdaf18b965
tairitsu86 <113744248+tairitsu86@users.noreply.github.com>
2023-08-31 14:38:12 +0800

    Add files via upload

---
+++ device-database/src/main/java/gradutionProject/IoTSystem/devicedatabase/DeviceDatabaseApplication.java
@@ -0,0 +1,13 @@
+package gradutionProject.IoTSystem.devicedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceDatabaseApplication.class, args);
+	}
+
+}
---
+++ device-database/src/test/java/gradutionProject/IoTSystem/devicedatabase/DeviceDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package gradutionProject.IoTSystem.devicedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
