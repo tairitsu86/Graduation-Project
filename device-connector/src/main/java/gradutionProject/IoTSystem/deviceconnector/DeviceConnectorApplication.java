ba90ef8ef89602b28befcc9543ce904abe287e79
tairitsu86 <113744248+tairitsu86@users.noreply.github.com>
2023-08-31 14:51:46 +0800

    Add files via upload

---
+++ device-connector/src/main/java/gradutionProject/IoTSystem/deviceconnector/DeviceConnectorApplication.java
@@ -0,0 +1,13 @@
+package gradutionProject.IoTSystem.deviceconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceConnectorApplication.class, args);
+	}
+
+}
---
+++ device-connector/src/test/java/gradutionProject/IoTSystem/deviceconnector/DeviceConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package gradutionProject.IoTSystem.deviceconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
