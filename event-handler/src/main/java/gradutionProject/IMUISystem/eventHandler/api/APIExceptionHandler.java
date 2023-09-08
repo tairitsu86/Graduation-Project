ea3d892a12a10f0dac6f40bbcc77b7270582c6e5
tairitsu86 <benimiku9@gmail.com>
2023-09-08 14:09:19 +0800

    change dir

--- event-handler/src/main/java/gradutionProject/IMUISystem/eventHandler/api/APIExceptionHandler.java
+++ event-handler/src/main/java/gradutionProject/IMUISystem/eventHandler/api/APIExceptionHandler.java
@@ -1,5 +1,7 @@
-package gradutionProject.IMUISystem.eventHandler.api.exception;
+package gradutionProject.IMUISystem.eventHandler.api;
 
+import gradutionProject.IMUISystem.eventHandler.api.exception.EventAlreadyExistException;
+import gradutionProject.IMUISystem.eventHandler.api.exception.EventNotExistException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
