b4546279c177a7203490e6d66ad7e238ace4646f
tairitsu86 <benimiku9@gmail.com>
2023-09-12 14:28:58 +0800

    add device state compose primary key

---
+++ device-connector/src/main/java/gradutionProject/IoTSystem/deviceconnector/entity/DeviceStateId.java
@@ -0,0 +1,20 @@
+package gradutionProject.IoTSystem.deviceconnector.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Embeddable
+public class DeviceStateId {
+    @Column(name = "device_id")
+    private String deviceId;
+
+    @Column(name = "state_name")
+    private String stateName;
+}
