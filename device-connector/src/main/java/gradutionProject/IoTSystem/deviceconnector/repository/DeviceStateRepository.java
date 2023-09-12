67388bad2d43d20a36911c4784d532acdcc8730f
tairitsu86 <benimiku9@gmail.com>
2023-09-12 14:29:30 +0800

    add costumer query

--- device-connector/src/main/java/gradutionProject/IoTSystem/deviceconnector/repository/DeviceStateRepository.java
+++ device-connector/src/main/java/gradutionProject/IoTSystem/deviceconnector/repository/DeviceStateRepository.java
@@ -1,7 +1,9 @@
 package gradutionProject.IoTSystem.deviceconnector.repository;
 
 import gradutionProject.IoTSystem.deviceconnector.dto.DeviceFunction;
+import gradutionProject.IoTSystem.deviceconnector.dto.DeviceStateDto;
 import gradutionProject.IoTSystem.deviceconnector.entity.DeviceState;
+import gradutionProject.IoTSystem.deviceconnector.entity.DeviceStateId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,10 +11,11 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface DeviceStateRepository extends JpaRepository<DeviceState,String> {
-    @Query(value = "SELECT * FROM device_functions WHERE device_id = '?1'",nativeQuery = true)
-    List<DeviceFunction> getFunctions(String device_id);
+public interface DeviceStateRepository extends JpaRepository<DeviceState, DeviceStateId> {
+    @Query(value = "SELECT [state_name],[state_value] FROM [device_states] WHERE [device_id] = '?1'",nativeQuery = true)
+    List<DeviceStateDto> getState(String deviceId);
 
-    @Query(value = "SELECT function_name FROM device_functions WHERE device_id = '?1'",nativeQuery = true)
-    List<String> getFunctionNames(String device_id);
+    @Query(value = "SELECT [state_name],[state_value] FROM [device_states] WHERE [device_id] = '?1' AND [state_name] = '?2'",nativeQuery = true)
+    List<DeviceStateDto> getState(String deviceId,String stateName);
+
 }
