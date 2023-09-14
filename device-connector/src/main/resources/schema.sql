da29f1add3b6dea6168771d7e87a19c2e9d6cc54
tairitsu86 <benimiku9@gmail.com>
2023-09-14 14:09:19 +0800

    add an insert time column

--- device-connector/src/main/resources/schema.sql
+++ device-connector/src/main/resources/schema.sql
@@ -1,10 +1,10 @@
-
 IF OBJECT_ID(N'device_states',N'U') IS NULL
 	BEGIN
 		CREATE TABLE [device_states](
 			[device_id] [varchar](255) NOT NULL,
 			[state_name] [varchar](255) NOT NULL,
 			[state_value] [varchar](255) NOT NULL,
+			[insert_time] [datetime2] NOT NULL CONSTRAINT [insert_time_default] DEFAULT(SYSDATETIME()),
 			CONSTRAINT [device_states_pk] PRIMARY KEY([device_id],[state_name])
 		)
 	END;
