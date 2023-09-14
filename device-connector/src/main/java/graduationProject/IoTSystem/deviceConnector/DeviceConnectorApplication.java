a699225dff1f8a58d23d678ccbe2fbca881ee49c
tairitsu86 <benimiku9@gmail.com>
2023-09-14 14:09:02 +0800

    add object mapper bean

--- device-connector/src/main/java/graduationProject/IoTSystem/deviceConnector/DeviceConnectorApplication.java
+++ device-connector/src/main/java/graduationProject/IoTSystem/deviceConnector/DeviceConnectorApplication.java
@@ -1,5 +1,6 @@
 package graduationProject.IoTSystem.deviceConnector;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,5 +16,9 @@ public class DeviceConnectorApplication {
 	@Bean
 	public RestTemplate createRestTemplate(){
 		return new RestTemplate();
+	}
+	@Bean
+	public ObjectMapper createObjectMapper(){
+		return new ObjectMapper();
 	}
 }
