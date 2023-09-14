046fcf794d0755b89d1fc140bd836a9932d3ae2c
tairitsu86 <benimiku9@gmail.com>
2023-09-14 16:32:21 +0800

    add new api and api exception

---
+++ device-database/src/main/java/gradutionProject/IoTSystem/deviceDatabase/dto/DeviceControlDto.java
@@ -0,0 +1,16 @@
+package gradutionProject.IoTSystem.deviceDatabase.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class DeviceControlDto {
+    private String deviceId;
+    private int functionId;
+    private String functionPara;
+}
