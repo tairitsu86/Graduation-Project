02c31b0d6564ae66f5463896973bb8197bb9961d
tairitsu86 <benimiku9@gmail.com>
2023-09-17 12:52:02 +0800

    implement repository service

--- device-database/src/main/java/gradutionProject/IoTSystem/deviceDatabase/repository/DeviceRepositoryService.java
+++ device-database/src/main/java/gradutionProject/IoTSystem/deviceDatabase/repository/DeviceRepositoryService.java
@@ -3,12 +3,14 @@ package gradutionProject.IoTSystem.deviceDatabase.repository;
 import gradutionProject.IoTSystem.deviceDatabase.dto.GetDevicesDto;
 import gradutionProject.IoTSystem.deviceDatabase.dto.GetDeviceDto;
 import gradutionProject.IoTSystem.deviceDatabase.entity.Device;
+import gradutionProject.IoTSystem.deviceDatabase.entity.FunctionType;
 
 import java.util.List;
 
 public interface DeviceRepositoryService {
     List<GetDevicesDto> getDevices(String username);
-    GetDeviceDto getDevice(String username, String deviceId);
+    GetDeviceDto getDevice(String deviceId);
     void updateDeviceInfo(Device device);
     boolean checkPermission(String username,String deviceId);
+    FunctionType getFunctionType(String deviceId,int functionId);
 }
--- device-database/src/main/java/gradutionProject/IoTSystem/deviceDatabase/repository/DeviceRepositoryServiceImpl.java
+++ device-database/src/main/java/gradutionProject/IoTSystem/deviceDatabase/repository/DeviceRepositoryServiceImpl.java
@@ -3,6 +3,7 @@ package gradutionProject.IoTSystem.deviceDatabase.repository;
 import gradutionProject.IoTSystem.deviceDatabase.dto.GetDeviceDto;
 import gradutionProject.IoTSystem.deviceDatabase.dto.GetDevicesDto;
 import gradutionProject.IoTSystem.deviceDatabase.entity.Device;
+import gradutionProject.IoTSystem.deviceDatabase.entity.FunctionType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,24 +13,31 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DeviceRepositoryServiceImpl implements DeviceRepositoryService{
     private final DeviceRepository deviceRepository;
-    //TODO
     @Override
     public List<GetDevicesDto> getDevices(String username) {
-        return null;
+        return deviceRepository.getDeviceByUsername(username);
     }
 
     @Override
-    public GetDeviceDto getDevice(String username, String deviceId) {
-        return null;
+    public GetDeviceDto getDevice(String deviceId) {
+        GetDeviceDto getDeviceDto = deviceRepository.getDeviceByDeviceId(deviceId).isEmpty()?null:deviceRepository.getDeviceByDeviceId(deviceId).get();
+        return getDeviceDto;
     }
 
     @Override
     public void updateDeviceInfo(Device device) {
-
+        deviceRepository.save(device);
     }
 
     @Override
     public boolean checkPermission(String username, String deviceId) {
+        if(deviceRepository.getDeviceOwner(deviceId).isEmpty()) return false;
+        if(deviceRepository.getDeviceOwner(deviceId).get().equals(username)) return true;
         return false;
+    }
+
+    @Override
+    public FunctionType getFunctionType(String deviceId, int functionId) {
+        return deviceRepository.getFunctionType(deviceId,functionId).isEmpty()?null:deviceRepository.getFunctionType(deviceId,functionId).get();
     }
 }
