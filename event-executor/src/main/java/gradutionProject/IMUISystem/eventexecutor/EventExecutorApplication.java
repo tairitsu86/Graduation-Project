8db4607a288fe9b7745785449355ec688d6db1d1
tairitsu86 <113744248+tairitsu86@users.noreply.github.com>
2023-09-18 10:04:59 +0800

    Add files via upload

---
+++ event-executor/src/main/java/gradutionProject/IMUISystem/eventexecutor/EventExecutorApplication.java
@@ -0,0 +1,13 @@
+package gradutionProject.IMUISystem.eventexecutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventExecutorApplication.class, args);
+	}
+
+}
---
+++ event-executor/src/test/java/gradutionProject/IMUISystem/eventexecutor/EventExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package gradutionProject.IMUISystem.eventexecutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventExecutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
