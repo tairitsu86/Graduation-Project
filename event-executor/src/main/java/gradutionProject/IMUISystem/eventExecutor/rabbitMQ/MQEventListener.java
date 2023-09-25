56c2efafdf3d3e6090e6c03797b7120d76307fd8
tairitsu86 <benimiku9@gmail.com>
2023-09-25 11:38:24 +0800

    add response to user

--- event-executor/src/main/java/gradutionProject/IMUISystem/eventExecutor/rabbitMQ/MQEventListener.java
+++ event-executor/src/main/java/gradutionProject/IMUISystem/eventExecutor/rabbitMQ/MQEventListener.java
@@ -1,13 +1,15 @@
 package gradutionProject.IMUISystem.eventExecutor.rabbitMQ;
 
 import gradutionProject.IMUISystem.eventExecutor.dto.ExecuteEventDto;
-import gradutionProject.IMUISystem.eventExecutor.repository.APIDataRepository;
 import gradutionProject.IMUISystem.eventExecutor.repository.RepositoryService;
 import gradutionProject.IMUISystem.eventExecutor.request.RestRequestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 
 import static gradutionProject.IMUISystem.eventExecutor.rabbitMQ.RabbitmqConfig.executeEventQueue;
 
@@ -17,6 +19,7 @@ import static gradutionProject.IMUISystem.eventExecutor.rabbitMQ.RabbitmqConfig.
 public class MQEventListener {
     private final RepositoryService repositoryService;
     private final RestRequestService restRequestService;
+    private final MQEventPublisher mqEventPublisher;
 
     @RabbitListener(queues={executeEventQueue})
     public void receive(ExecuteEventDto executeEventDto) {
@@ -24,9 +27,22 @@ public class MQEventListener {
         try {
             switch (repositoryService.getEventType(executeEventDto.getEventName())){
                 case MQ -> {}
-                case API -> restRequestService.sendEventRequest(repositoryService.getApiDataByEventName(executeEventDto.getEventName()),executeEventDto.getVariables());
+                case API -> {
+                    ResponseEntity<String> response =
+                            restRequestService.sendEventRequest(
+                                    repositoryService.getApiDataByEventName(
+                                            executeEventDto.getEventName()
+                                    ),
+                                    executeEventDto.getVariables()
+                            );
+                    mqEventPublisher.notifyUser(
+                            new ArrayList<String>(){{executeEventDto.getExecutor();}},
+                            repositoryService.getNotifyData(executeEventDto.getEventName()),
+                            response.getBody());
+                }
                 default -> {return;}
             }
+
         }catch (NullPointerException e){
             log.info("Some field be null!");
         }
