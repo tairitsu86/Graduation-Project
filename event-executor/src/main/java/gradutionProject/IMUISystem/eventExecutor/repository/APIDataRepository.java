f5230107fd75c66240ef7c60961c7920c49a4eab
tairitsu86 <benimiku9@gmail.com>
2023-09-25 20:07:10 +0800

    rename file

--- event-executor/src/main/java/gradutionProject/IMUISystem/eventExecutor/repository/APIDataRepository.java
+++
@@ -1,8 +0,0 @@
-package gradutionProject.IMUISystem.eventExecutor.repository;
-
-import gradutionProject.IMUISystem.eventExecutor.entity.APIData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface APIDataRepository extends JpaRepository<APIData,String> {
-
-}
