b0f49d69b51993c7ca9d7064f11027589ef45b56
tairitsu86 <benimiku9@gmail.com>
2023-09-28 09:08:30 +0800

    add @OrderColumn

--- event-handler/src/main/java/gradutionProject/IMUISystem/eventHandler/entity/UserState.java
+++ event-handler/src/main/java/gradutionProject/IMUISystem/eventHandler/entity/UserState.java
@@ -18,10 +18,13 @@ import java.util.Map;
 public class UserState {
     @EmbeddedId
     private IMUserData imUserData;
+
     @Column(name = "username")
     private String username;
+
     @Column(name = "event_name")
     private String eventName;
+
     @ElementCollection
     @JoinTable(
             name = "user_state_data",
@@ -31,7 +34,9 @@ public class UserState {
             }
     )
     @Column(name = "data")
+    @OrderColumn
     private List<String> data;
+
     @ElementCollection
     @JoinTable(
             name = "user_state_variables",
