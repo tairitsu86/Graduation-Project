006a8bc96ccab4e404576537fb0f459b6a579289
tairitsu86 <benimiku9@gmail.com>
2023-10-12 13:38:40 +0800

    add JsonProperty

--- event-handler/src/main/java/gradutionProject/IMUISystem/eventHandler/dto/CustomizeEventDto.java
+++ event-handler/src/main/java/gradutionProject/IMUISystem/eventHandler/dto/CustomizeEventDto.java
@@ -1,5 +1,6 @@
 package gradutionProject.IMUISystem.eventHandler.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +16,8 @@ public class CustomizeEventDto {
     private String eventName;
     private String description;
     private List<String> variables;
+    @JsonProperty("commConfig")
     private CommConfigDto commConfigDto;
+    @JsonProperty("notifyConfig")
     private NotifyConfigDto notifyConfigDto;
 }
