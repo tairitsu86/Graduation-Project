fe85c7a4b2cfa2cdba6475e4d54ab8bee8a45305
tairitsu86 <benimiku9@gmail.com>
2023-10-12 13:30:33 +0800

    add objectMapper bean

--- event-handler/src/main/java/gradutionProject/IMUISystem/eventHandler/EventHandlerApplication.java
+++ event-handler/src/main/java/gradutionProject/IMUISystem/eventHandler/EventHandlerApplication.java
@@ -1,5 +1,6 @@
 package gradutionProject.IMUISystem.eventHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,5 +16,9 @@ public class EventHandlerApplication {
 	@Bean
 	public RestTemplate createRestTemplateBean(){
 		return new RestTemplate();
+	}
+	@Bean
+	public ObjectMapper createObjectMapperBean(){
+		return new ObjectMapper();
 	}
 }
