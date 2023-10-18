0649955a899c35ade5e89391f6543368a2f60403
tairitsu86 <113744248+tairitsu86@users.noreply.github.com>
2023-10-18 13:08:59 +0800

    Add files via upload

---
+++ telegram-service/src/test/java/gradutionProject/IMIGSystem/telegramservice/TelegramServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gradutionProject.IMIGSystem.telegramservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
