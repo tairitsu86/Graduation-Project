b0b8c2732a8a512ea94dcbd1b42a67c6c1930625
tairitsu86 <benimiku9@gmail.com>
2023-10-18 14:22:29 +0800

    add sending function

--- line-service/src/main/java/gradutionProject/IMIGSystem/lineService/rabbitMQ/RabbitmqConfig.java
+++ line-service/src/main/java/gradutionProject/IMIGSystem/lineService/rabbitMQ/RabbitmqConfig.java
@@ -15,9 +15,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableRabbit
 public class RabbitmqConfig {
-    public static final String IMUI_IBC_EXCHANGE = "IM-UI-System_IM-base-communication_Exchange";
-
     public static final String IM_USER_MESSAGE_QUEUE = "IM-UI-System.IM-User-Message";
+    public static final String LINE_QUEUE = "IM-Integration-System.LINE";
+    public static final String IMUI_IBC_EXCHANGE = "IM-UI-System_IM-base-communication_Exchange";
+    public static final String IMIG_IPS_EXCHANGE = "IM-Integration-System_IM-platform-service_Exchange";
+
     @Bean
     public MessageConverter jsonMessageConverter(ObjectMapper objectMapper) {
         return new Jackson2JsonMessageConverter(objectMapper);
@@ -34,8 +36,21 @@ public class RabbitmqConfig {
         return new Queue(IM_USER_MESSAGE_QUEUE,false);
     }
     @Bean
-    public Binding binding(@Qualifier("IMUI_IBC") TopicExchange exchange) {
+    public Binding bindingIMUSerMessage(@Qualifier("IMUI_IBC") TopicExchange exchange) {
         return BindingBuilder.bind(imUserMessageQueue()).to(exchange).with(IM_USER_MESSAGE_QUEUE);
+    }
+
+    @Bean("IMIG_IPS")
+    public TopicExchange exchangeIMIG_IPS() {
+        return new TopicExchange(IMIG_IPS_EXCHANGE);
+    }
+    @Bean
+    public Queue lineQueue() {
+        return new Queue(LINE_QUEUE,false);
+    }
+    @Bean
+    public Binding bindingLineQueue(@Qualifier("IMIG_IPS") TopicExchange exchange) {
+        return BindingBuilder.bind(lineQueue()).to(exchange).with(LINE_QUEUE);
     }
 
 }
---
+++ line-service/src/main/java/gradutionProject/IMIGSystem/lineService/service/IMService.java
@@ -0,0 +1,5 @@
+package gradutionProject.IMIGSystem.lineService.service;
+
+public interface IMService {
+    void SendTextMessage(String userId,String message);
+}
