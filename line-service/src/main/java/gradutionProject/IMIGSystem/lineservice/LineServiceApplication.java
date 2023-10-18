b1df18683eb23b24fce731c57df93c6af74c11b1
tairitsu86 <113744248+tairitsu86@users.noreply.github.com>
2023-10-18 13:08:31 +0800

    Add files via upload

---
+++ line-service/src/main/java/gradutionProject/IMIGSystem/lineservice/LineServiceApplication.java
@@ -0,0 +1,13 @@
+package gradutionProject.IMIGSystem.lineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineServiceApplication.class, args);
+	}
+
+}
