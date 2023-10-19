0cc3ddda6d00e2e62239d0cd83694a517575ea1d
tairitsu86 <benimiku9@gmail.com>
2023-10-19 18:27:12 +0800

    add menu description template

--- event-executor/src/main/java/graduationProject/IMUISystem/eventExecutor/entity/MenuConfig.java
+++ event-executor/src/main/java/graduationProject/IMUISystem/eventExecutor/entity/MenuConfig.java
@@ -13,7 +13,7 @@ import java.util.Map;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MenuConfig {
-    private String description;
+    private String descriptionTemplate;
     private String displayNameTemplate;
     private String nextEvent;
     private List<MenuVariable> variables;
