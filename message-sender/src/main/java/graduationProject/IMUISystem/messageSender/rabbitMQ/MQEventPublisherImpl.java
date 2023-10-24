a6723d617ff87c01757007ffe857848b75a626d2
tairitsu86 <benimiku9@gmail.com>
2023-10-24 18:11:12 +0800

    add log

--- message-sender/src/main/java/graduationProject/IMUISystem/messageSender/rabbitMQ/MQEventPublisherImpl.java
+++ message-sender/src/main/java/graduationProject/IMUISystem/messageSender/rabbitMQ/MQEventPublisherImpl.java
@@ -2,6 +2,7 @@ package graduationProject.IMUISystem.messageSender.rabbitMQ;
 
 import graduationProject.IMUISystem.messageSender.dto.SendingServiceDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
@@ -9,11 +10,13 @@ import static graduationProject.IMUISystem.messageSender.rabbitMQ.RabbitmqConfig
 
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class MQEventPublisherImpl implements MQEventPublisher{
     private final RabbitTemplate rabbitTemplate;
     @Override
     public void publishSendingServiceEvent(String platform, SendingServiceDto sendingServiceDto) {
         rabbitTemplate.convertAndSend(IMIG_IPS_EXCHANGE, String.format("IM-Integration-System.%s", platform), sendingServiceDto);
+        log.info("Send to exchange [{}] with routingKey [{}], event [{}]", IMIG_IPS_EXCHANGE, String.format("IM-Integration-System.%s", platform), sendingServiceDto);
     }
 }
