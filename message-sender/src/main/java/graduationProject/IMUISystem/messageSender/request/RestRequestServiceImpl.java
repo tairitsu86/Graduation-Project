43659117d34b7fe3e4c60b95a184005157128636
tairitsu86 <benimiku9@gmail.com>
2023-10-24 18:07:56 +0800

    debug

--- message-sender/src/main/java/graduationProject/IMUISystem/messageSender/request/RestRequestServiceImpl.java
+++ message-sender/src/main/java/graduationProject/IMUISystem/messageSender/request/RestRequestServiceImpl.java
@@ -3,6 +3,7 @@ package graduationProject.IMUISystem.messageSender.request;
 import graduationProject.IMUISystem.messageSender.dto.IMUserData;
 import graduationProject.IMUISystem.messageSender.dto.LoginUserDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class RestRequestServiceImpl implements RestRequestService {
     @Value("${my_env.loginTrackerUrl}")
@@ -22,10 +24,16 @@ public class RestRequestServiceImpl implements RestRequestService {
         List<IMUserData> imUserDataList = new ArrayList<>();
         LoginUserDto loginUserDto;
         for(String username:usernameList){
-            loginUserDto = restTemplate.getForObject(String.format("%s/users/%s",LOGIN_TRACKER_URL,username), LoginUserDto.class);
-            if(loginUserDto==null||loginUserDto.getImUserData()==null) continue;
-            imUserDataList.add(loginUserDto.getImUserData());
+            try{
+                loginUserDto = restTemplate.getForObject(String.format("%s/users/%s",LOGIN_TRACKER_URL,username), LoginUserDto.class);
+                if(loginUserDto==null||loginUserDto.getImUserData()==null) continue;
+                imUserDataList.add(loginUserDto.getImUserData());
+            }catch (Exception e){
+                log.info("getIMData error with: {}", e.getMessage(), e);
+            }
         }
+
+
         return imUserDataList;
     }
 }
