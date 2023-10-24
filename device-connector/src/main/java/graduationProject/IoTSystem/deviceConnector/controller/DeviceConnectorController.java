4e7c69fda01c559f942be1277ed0e6a2ddf20db6
tairitsu86 <benimiku9@gmail.com>
2023-10-24 13:52:31 +0800

    add newDevice api

--- device-connector/src/main/java/graduationProject/IoTSystem/deviceConnector/controller/DeviceConnectorController.java
+++ device-connector/src/main/java/graduationProject/IoTSystem/deviceConnector/controller/DeviceConnectorController.java
@@ -1,14 +1,19 @@
 package graduationProject.IoTSystem.deviceConnector.controller;
 
+import graduationProject.IoTSystem.deviceConnector.dto.NewDeviceDto;
+import graduationProject.IoTSystem.deviceConnector.mqtt.MQTTConfig;
 import graduationProject.IoTSystem.deviceConnector.repository.DeviceStateHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequiredArgsConstructor
 public class DeviceConnectorController {
     private final DeviceStateHistoryRepository deviceStateHistoryRepository;
+    private final MQTTConfig mqttConfig;
     @GetMapping("/")
     @ResponseStatus(HttpStatus.OK)
     public String home(){
@@ -18,6 +23,14 @@ public class DeviceConnectorController {
     @GetMapping("/devices/{deviceId}/states")
     private Object getDeviceAllStates(@PathVariable String deviceId){
         return deviceStateHistoryRepository.getDeviceAllStates(deviceId);
+    }
+    @GetMapping("/devices/new")
+    public NewDeviceDto newDevice(){
+        return NewDeviceDto.builder()
+                .id(UUID.randomUUID().toString())
+                .mqttHostIp(mqttConfig.getMQTT_HOST_IP())
+                .mqttHostPort(mqttConfig.getMQTT_HOST_PORT())
+                .build();
     }
 }
 
