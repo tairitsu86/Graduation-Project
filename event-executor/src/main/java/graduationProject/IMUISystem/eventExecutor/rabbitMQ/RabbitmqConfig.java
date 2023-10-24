a01394fa87721047cfcc77ef63f8be6a6ee5e60d
tairitsu86 <benimiku9@gmail.com>
2023-10-24 11:04:37 +0800

    change SYS_NTF_EXCHANGE to fan out exchange, because the wildcard in topic exchange not work

--- event-executor/src/main/java/graduationProject/IMUISystem/eventExecutor/rabbitMQ/RabbitmqConfig.java
+++ event-executor/src/main/java/graduationProject/IMUISystem/eventExecutor/rabbitMQ/RabbitmqConfig.java
@@ -1,10 +1,7 @@
 package graduationProject.IMUISystem.eventExecutor.rabbitMQ;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -68,8 +65,8 @@ public class RabbitmqConfig {
 
 
     @Bean("SYS_NTF")
-    public TopicExchange exchangeSYS_NTF() {
-        return new TopicExchange(SYS_NTF_EXCHANGE);
+    public FanoutExchange exchangeSYS_NTF() {
+        return new FanoutExchange(SYS_NTF_EXCHANGE);
     }
     @Bean
     public Queue notifyUserQueue() {
