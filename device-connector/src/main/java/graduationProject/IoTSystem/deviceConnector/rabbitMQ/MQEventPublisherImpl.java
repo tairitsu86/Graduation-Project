046c9b4fd95944d4b343281d002636f14293d210
tairitsu86 <benimiku9@gmail.com>
2023-10-24 11:04:10 +0800

    change SYS_NTF_EXCHANGE to fan out exchange, because the wildcard in topic exchange not work

--- device-connector/src/main/java/graduationProject/IoTSystem/deviceConnector/rabbitMQ/MQEventPublisherImpl.java
+++ device-connector/src/main/java/graduationProject/IoTSystem/deviceConnector/rabbitMQ/MQEventPublisherImpl.java
@@ -2,6 +2,7 @@ package graduationProject.IoTSystem.deviceConnector.rabbitMQ;
 
 import graduationProject.IoTSystem.deviceConnector.dto.DeviceStateDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
@@ -12,17 +13,20 @@ import static graduationProject.IoTSystem.deviceConnector.rabbitMQ.RabbitmqConfi
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MQEventPublisherImpl implements MQEventPublisher{
     private final RabbitTemplate rabbitTemplate;
     @Override
     public void publishDeviceStateEvent(DeviceStateDto deviceStateDto) {
         deviceStateDto.setNotificationType("device-state");
         deviceStateDto.setFrom("device-connector");
-        rabbitTemplate.convertAndSend(SYS_NTF_EXCHANGE, "",deviceStateDto);
+        rabbitTemplate.convertAndSend(SYS_NTF_EXCHANGE, "#", deviceStateDto);
+        log.info("Send to exchange [{}] with routingKey [{}], event [{}]", SYS_NTF_EXCHANGE, "#", deviceStateDto);
     }
 
     @Override
     public void publishDeviceInfoEvent(Map<String,Object> deviceInfoDto) {
-        rabbitTemplate.convertAndSend(IoT_DBC_EXCHANGE, DEVICE_INFO_QUEUE,deviceInfoDto);
+        rabbitTemplate.convertAndSend(IoT_DBC_EXCHANGE, DEVICE_INFO_QUEUE, deviceInfoDto);
+        log.info("Send to exchange [{}] with routingKey [{}], event [{}]", IoT_DBC_EXCHANGE, DEVICE_INFO_QUEUE, deviceInfoDto);
     }
 }
