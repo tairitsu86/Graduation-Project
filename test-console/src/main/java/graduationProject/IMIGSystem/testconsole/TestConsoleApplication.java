ba104b88bea689b535be40fa601627971b8eda75
tairitsu86 <113744248+tairitsu86@users.noreply.github.com>
2023-11-01 19:01:17 +0800

    Add files via upload

---
+++ test-console/src/main/java/graduationProject/IMIGSystem/testconsole/TestConsoleApplication.java
@@ -0,0 +1,13 @@
+package graduationProject.IMIGSystem.testconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestConsoleApplication.class, args);
+	}
+
+}
---
+++ test-console/src/test/java/graduationProject/IMIGSystem/testconsole/TestConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package graduationProject.IMIGSystem.testconsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestConsoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
